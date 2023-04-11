e8365b3b6af6cba80cd39a9eab574392ec7f3d9c
sannarossang <sanna.rossang@gmail.com>
2023-04-11 18:39:44 +0200

    (feature): added gameboard + functionality for it

---
+++ src/components/GameBoard.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { Player } from "../models/Player";
+
+// const state = ref<Player>({ name: "Sanna", id: 2 });
+
+const handleClick = (num: number, event: MouseEvent) => {
+  console.log("Du klickade på", num, event);
+};
+</script>
+
+<template>
+  <div>Tic Tac Toe by Rossäng</div>
+  <div class="board">
+    <div class="square" v-for="num of 9" :key="num" @click="handleClick(num, $event)">
+      {{ num }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.board {
+  width: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+}
+.square {
+  background: pink;
+  border: 1px dotted purple;
+  height: 150px;
+  width: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/components/TicTac.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import GameBoard from "./GameBoard.vue";
+</script>
+
+<template>
+  <GameBoard />
+</template>
+
+<style scoped></style>
